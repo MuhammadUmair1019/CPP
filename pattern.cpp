fe40772e0d861545d9f52fbd759a74be4cb57dd2
Muhammad Umair <Muhammad.umair1019@gmail.com>
2023-09-17 18:13:48 +0500

    pattern-10/11

--- pattern.cpp
+++ pattern.cpp
@@ -224,7 +224,42 @@ void pattern9(int n)
   }
 }
 // --------------------------------------------------------------------------
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
 void pattern10(int n)
+{
+
+  for (int i = 1; i <= n; i++)
+  {
+
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+
+    cout << endl;
+  }
+
+  for (int i = n - 1; i >= 1; i--)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+
+    cout << endl;
+  }
+}
+
+// efficient way 
+void pattern11(int n)
 {
   for (int i = -n + 1; i < n; i++)
   {
@@ -234,6 +269,9 @@ void pattern10(int n)
     cout << string(stars - 1, '*') << endl;
   }
 }
+// --------------------------------------------------------------------------
+
+
 int main()
 {
   int n;
@@ -259,4 +297,6 @@ int main()
   pattern9(n);
   cout << endl;
   pattern10(n);
+  cout << endl;
+  pattern11(n);
 }

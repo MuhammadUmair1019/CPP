54c8be83cbb859157207d6cb0fa24e6ff11f1261
Muhammad Umair <Muhammad.umair1019@gmail.com>
2023-09-19 00:45:38 +0500

    more pattrens

--- pattern.cpp
+++ pattern.cpp
@@ -314,6 +314,217 @@ void pattern13(int n)
   }
 }
 // --------------------------------------------------------------------------
+// 1        1
+// 12      21
+// 123    321
+// 1234  4321
+// 1234554321
+void pattern14(int n)
+{
+  int spaces = 2 * (n - 1);
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << j;
+    }
+
+    for (int j = 1; j <= spaces; j++)
+    {
+      cout << " ";
+    }
+
+    for (int j = i; j >= 1; j--)
+    {
+      cout << j;
+    }
+
+    cout << endl;
+    spaces -= 2;
+  }
+}
+// --------------------------------------------------------------------------
+// 1
+// 23
+// 456
+// 78910
+// 1112131415
+void pattern15(int n)
+{
+  int count = 1;
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << count;
+      count++;
+    }
+
+    cout << endl;
+  }
+}
+// --------------------------------------------------------------------------
+// A B
+// A B C
+// A B C D
+// A B C D E
+// A B C D E F
+void pattern16(int n)
+{
+  char alphabet = 'A';
+  for (int i = 1; i <= n; i++)
+  {
+    for (char ch = 'A'; ch <= alphabet + i; ch++)
+    {
+      cout << ch << " ";
+    }
+
+    cout << endl;
+  }
+}
+
+// --------------------------------------------------------------------------
+// A B C D E F
+// A B C D E
+// A B C D
+// A B C
+// A B
+void pattern17(int n)
+{
+  char alphabet = 'A';
+  for (int i = n; i >= 1; i--)
+  {
+    for (char ch = 'A'; ch <= alphabet + i; ch++)
+    {
+      cout << ch << " ";
+    }
+
+    cout << endl;
+  }
+}
+// --------------------------------------------------------------------------
+// A
+// B B
+// C C C
+// D D D D
+// E E E E E
+void pattern18(int n)
+{
+  char alphabet = 'A';
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << alphabet << " ";
+    }
+
+    cout << endl;
+    alphabet++;
+  }
+}
+// --------------------------------------------------------------------------
+//     A
+//    ABA
+//   ABCBA
+//  ABCDCBA
+// ABCDEDCBA
+
+void pattern19(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      cout << " ";
+    }
+
+    char ch = 'A';
+    int breakPoint = (2 * i + 1) / 2;
+    for (int j = 0; j < 2 * i + 1; j++)
+    {
+      cout << ch;
+      j < breakPoint ? ch++ : ch--;
+    }
+
+    cout << endl;
+  }
+}
+// --------------------------------------------------------------------------
+// E
+// D E
+// C D E
+// B C D E
+// A B C D E
+void pattern20(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (char ch = ('A' + n - 1) - i; ch <= ('A' + n - 1); ch++)
+    {
+      cout << ch << " ";
+    }
+
+    cout << endl;
+  }
+}
+// --------------------------------------------------------------------------
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+void pattern21(int n)
+{
+  int spaces = 0;
+  for (int i = n; i >= 1; i--)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+
+    for (int j = 1; j <= spaces; j++)
+    {
+      cout << " ";
+    }
+
+    for (int j = i; j >= 1; j--)
+    {
+      cout << "*";
+    }
+
+    cout << endl;
+    spaces += 2;
+  }
+
+  int spaces2 = 2 * (n - 1);
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+
+    for (int j = 1; j <= spaces2; j++)
+    {
+      cout << " ";
+    }
+
+    for (int j = i; j >= 1; j--)
+    {
+      cout << "*";
+    }
+
+    cout << endl;
+    spaces2 -= 2;
+  }
+}
+// --------------------------------------------------------------------------
 
 int main()
 {
@@ -346,5 +557,20 @@ int main()
   pattern12(n);
   cout << endl;
   pattern13(n);
-  
+  cout << endl;
+  pattern14(n);
+  cout << endl;
+  pattern15(n);
+  cout << endl;
+  pattern16(n);
+  cout << endl;
+  pattern17(n);
+  cout << endl;
+  pattern18(n);
+  cout << endl;
+  pattern19(n);
+  cout << endl;
+  pattern20(n);
+  cout << endl;
+  pattern21(n);
 }

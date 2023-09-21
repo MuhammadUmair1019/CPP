ee8a3eb6ca27bc6f7ca2f3684e962c619f23c385
Muhammad Umair <Muhammad.umair1019@gmail.com>
2023-09-21 23:29:36 +0500

    pattrens

--- pattern.cpp
+++ pattern.cpp
@@ -525,6 +525,97 @@ void pattern21(int n)
   }
 }
 // --------------------------------------------------------------------------
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+
+void pattern22(int n)
+{
+  int spaces = 2 * n - 2;
+  for (int i = 1; i <= 2 * n - 1; i++)
+  {
+    int stars = i;
+    if (i > n)
+      stars = 2 * n - i;
+
+    for (int j = 1; j <= stars; j++)
+    {
+      cout << "*";
+    }
+
+    for (int j = 1; j <= spaces; j++)
+    {
+      cout << " ";
+    }
+
+    for (int j = 1; j <= stars; j++)
+    {
+      cout << "*";
+    }
+
+    cout << endl;
+    if (i < n)
+      spaces -= 2;
+    else
+      spaces += 2;
+  }
+}
+// --------------------------------------------------------------------------
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+void pattern23(int n)
+{
+
+  for (int i = 0; i < n; i++)
+  {
+
+    for (int j = 0; j < n; j++)
+    {
+      if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+        cout << "*";
+
+      else
+        cout << " ";
+    }
+
+    cout << endl;
+  }
+}
+
+// --------------------------------------------------------------------------
+// Input Format: N = 3
+// 3 3 3 3 3
+// 3 2 2 2 3
+// 3 2 1 2 3
+// 3 2 2 2 3
+// 3 3 3 3 3
+void pattern24(int n)
+{
+  for (int i = 0; i < 2 * n - 1; i++)
+  {
+    for (int j = 0; j < 2 * n - 1; j++)
+    {
+
+      int top = i;
+      int bottom = j;
+      int right = (2 * n - 2) - j;
+      int left = (2 * n - 2) - i;
+
+      cout << (n - min(min(top, bottom), min(left, right))) << " ";
+    }
+
+    cout << endl;
+  }
+}
 
 int main()
 {
@@ -573,4 +664,8 @@ int main()
   pattern20(n);
   cout << endl;
   pattern21(n);
+  cout << endl;
+  pattern22(n);
+  cout << endl;
+  pattern23(n);
 }
